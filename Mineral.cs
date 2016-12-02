91f43bde30f89da1162750c864e419c0da292632
EsauBz <produc.galofrantes@gmail.com>
2016-12-02 15:32:05 -0600

    Add files via upload

---
+++ Mineral.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scroll_fondo
+{
+    class Mineral
+    {
+        public Bitmap img = (Bitmap)Image.FromFile("mineral.png");
+
+        private int mapX, mapY;
+        private bool selected;
+        private int ancho;
+        private int alto;
+        private bool painted;
+        private int recurso;
+        private int DrawPercLife;
+        public Mineral()
+        {
+
+        }
+        public void disminuyeRecurso(int quita)
+        {
+            recurso -= quita;
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Timers;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scroll_fondo
+{
+    class Player
+    {
+        int CoordInicialMapX;
+        int CoordInicialMapY;
+        Random r = new Random();
+        public bool bando;
+        private List<Aldeano> aldeanos;
+        private List<UnidadMilitar> milicia;
+        private List<UnidadMilitar> jedis;
+        // lista de soldados
+        //lista de edificios
+        public int material;
+        public int food;
+        Aldeano al;
+
+        public Player()
+        {
+            aldeanos = new List<Aldeano>();
+            CoordInicialMapX = r.Next(0,3001); //random para aparecer al azar
+            CoordInicialMapY = r.Next(0, 3001);// random para aparecer al azar
+            for(int i = 0; i < 3; i++)
+            {
+                al = new Aldeano(CoordInicialMapX + r.Next(50), CoordInicialMapY + r.Next(50)); //coordenadas de inicio
+                aldeanos.Add(al);
+            }
+            //ageregar un soldado
+            // agregar un edicio centro urbano
+            food = 200;
+            material = 150;
+        }
+
+        public void AddAldeano(Aldeano al)
+        {
+            aldeanos.Add(al);                 
+        }
+        public List<Aldeano> getListAldeanos()
+        {
+            return aldeanos;
+        }
+        public int getCoordInicalMapX()
+        {
+            return CoordInicialMapX;
+        }
+        public int getCoordInicalMapY()
+        {
+            return CoordInicialMapY;
+        }
+        public void revisaRecogidaAldeanos(object obj, ElapsedEventHandler arg)
+        {
+
+        }
+    }
+}
